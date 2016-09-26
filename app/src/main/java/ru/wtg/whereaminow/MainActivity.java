a5e46d326635bb2479a50cb7518999ef9cd3ee9e
Tujger <tujger@gmail.com>
2016-09-25 22:07:56 -0400

    Development stage 2

--- app/src/main/java/ru/wtg/whereaminow/MainActivity.java
+++ app/src/main/java/ru/wtg/whereaminow/MainActivity.java
@@ -13,11 +13,9 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -25,10 +23,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.ArrayList;
+
+import ru.wtg.whereaminow.helpers.FabMenu;
+import ru.wtg.whereaminow.helpers.MyCamera;
+import ru.wtg.whereaminow.helpers.MyMarker;
+import ru.wtg.whereaminow.helpers.RelativeLayoutMap;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
@@ -38,8 +49,14 @@ public class MainActivity extends AppCompatActivity
 
     private LocationManager locationManager = null;
     private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
     private Intent intent;
+    private FabMenu buttons;
+    private ArrayList<MyMarker> markers = new ArrayList<>();
+    private ArrayList<MyCamera> cameras = new ArrayList<>();
+
     private boolean serviceBound;
+    private boolean locationManagerDefined = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +65,14 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.initAndSetOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -66,9 +83,11 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        buttons = (FabMenu) findViewById(R.id.fab);
 
         mapFragment.getMapAsync(this);
 
@@ -92,6 +111,7 @@ public class MainActivity extends AppCompatActivity
         super.onPause();
         if(locationManager != null && locationListener != null) {
             locationManager.removeUpdates(locationListener);
+            locationManagerDefined = false;
         }
     }
 
@@ -145,12 +165,26 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_start_and_share) {
-
-        } else if (id == R.id.nav_settings) {
-
+        switch(item.getItemId()) {
+            case R.id.nav_start_and_share:
+                break;
+            case R.id.nav_settings:
+                break;
+            case R.id.nav_traffic:
+                mMap.setTrafficEnabled(!mMap.isTrafficEnabled());
+                break;
+            case R.id.nav_satellite:
+                if (mMap.getMapType() != GoogleMap.MAP_TYPE_SATELLITE)
+                    mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                else
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.nav_terrain:
+                if (mMap.getMapType() != GoogleMap.MAP_TYPE_TERRAIN)
+                    mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                else
+                    mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -167,7 +201,7 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         System.out.println("onRequestPermissionsResult=" + requestCode);
         switch (requestCode) {
             case REQUEST_PERMISSION_LOCATION:
@@ -175,49 +209,6 @@ public class MainActivity extends AppCompatActivity
                 break;
         }
     }
-
-    public void onMapReadyPermitted() {
-        System.out.println("onMapReadyPermitted");
-
-        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
-            return;
-        }
-    }
-
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            System.out.println("onReceive");
-        }
-    };
-
-    private void enableLocationManager() {
-        if(locationManager != null && locationListener != null) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 1, 1, locationListener);
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 1, 1, locationListener);
-        }
-    }
-
-    private LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(Location location) {
-            System.out.println("onLocationChanged");
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            System.out.println("onProviderDisabled");
-        }
-
-        public void onProviderEnabled(String provider) {
-            System.out.println("onProviderEnabled");
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            System.out.println("onStatusChanged");
-        }
-    };
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
         public void onServiceConnected(ComponentName name, final IBinder binder) {
@@ -245,6 +236,257 @@ public class MainActivity extends AppCompatActivity
         public void onServiceDisconnected(ComponentName name) {
             serviceBound = false;
             System.out.println("onServiceDisconnected");
+        }
+    };
+
+    public void onMapReadyPermitted() {
+        System.out.println("onMapReadyPermitted");
+        MyMarker.setMap(mMap);
+        MyCamera.setMap(mMap);
+
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) && !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            System.err.println("NOT locationManager.isProviderEnabled");
+            return;
+        }
+        enableLocationManager();
+
+        cameras.add(new MyCamera(getApplicationContext()));
+        markers.add(new MyMarker(getApplicationContext()));
+        markers.get(0).setMyCamera(cameras.get(0));
+
+        mMap.setOnMarkerClickListener(onMarkerClickListener);
+        mMap.setOnCameraMoveStartedListener(cameras.get(0).onCameraMoveStartedListener);
+        mMap.setOnCameraMoveListener(cameras.get(0).onCameraMoveListener);
+        mMap.setOnCameraIdleListener(cameras.get(0).onCameraIdleListener);
+        mMap.setOnCameraMoveCanceledListener(cameras.get(0).onCameraMoveCanceledListener);
+        mMap.setOnMapClickListener(onMapClickListener);
+
+/*
+        ((RelativeLayoutMap)findViewById(R.id.layout_map)).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction()==0){
+                    cameras.get(0).setOrientation(MyCamera.ORIENTATION_STAY);
+                    System.out.println("motionEvent:"+motionEvent.getAction()+":"+motionEvent.getActionMasked());
+                }
+                return false;
+            }
+        });
+*/
+
+//        mMap.setMyLocationEnabled(true);
+//        mMap.setOnMyLocationButtonClickListener(cameras.get(0).onMyLocationButtonClickListener);
+
+        Location lastLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        if(lastLocation == null) lastLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        if(lastLocation == null) lastLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        if(lastLocation != null) {
+            markers.get(0).showDraft(lastLocation);
+            cameras.get(0).setLocation(lastLocation).update();
+        }
+
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if(lastLocation != null){
+                locationListener.onLocationChanged(lastLocation);
+            }
+        }
+
+        mMap.setBuildingsEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                System.out.println("MAP CLICK");
+            }
+        });
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setAllGesturesEnabled(true);
+        mMap.getUiSettings().setIndoorLevelPickerEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+        System.out.println("onReceive");
+        }
+    };
+
+    private void enableLocationManager() {
+        if(!locationManagerDefined && locationManager != null && locationListener != null) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, locationListener);
+            locationManagerDefined = true;
+        }
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            System.out.println("onLocationChanged");
+
+            if(!buttons.isInitialized()) {
+                adjustButtonsPositions();
+                initFab();
+            }
+
+            if(markers.size()>0) {
+                markers.get(0).addLocation(location);
+            }
+            if(cameras.size()>0) {
+                cameras.get(0).setLocation(location).update();
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            System.out.println("onProviderDisabled:"+provider);
+            buttons.hideMenuButton(true);
+        }
+
+        public void onProviderEnabled(String provider) {
+            System.out.println("onProviderEnabled:"+provider);
+            buttons.showMenuButton(true);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            System.out.println("onStatusChanged:"+provider);
+        }
+    };
+
+    private void initFab(){
+
+        buttons.setOnMenuButtonLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                System.out.println("fab.onLongClick");
+                return false;
+            }
+        });
+
+        buttons.initAndSetOnClickListener(onFabClickListener);
+
+    }
+
+    private View.OnClickListener onFabClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+                case -1:
+                    System.out.println("fab.onClick");
+
+                    if(!buttons.isOpened()) {
+                        buttons.removeAllMenuButtons();
+                        buttons.addMenuButton(buttons.startTrackingAndSendLink);
+                        buttons.addMenuButton(buttons.navigate);
+                        buttons.addMenuButton(buttons.cancelTracking);
+                    }
+                    buttons.toggle(true);
+
+                    break;
+                case R.id.fab_start_tracking_and_send_link:
+                    buttons.toggle(false);
+                    System.out.println("fab_start_tracking_and_send_link");
+                    Snackbar.make(findViewById(R.id.fab_layout), "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+
+                    break;
+                case R.id.fab_send_link:
+                    buttons.toggle(false);
+                    System.out.println("fab_send_link");
+                    break;
+                case R.id.fab_stop_tracking:
+                    buttons.toggle(false);
+                    System.out.println("fab_stop_tracking");
+                    break;
+                case R.id.fab_cancel_tracking:
+                    buttons.toggle(false);
+                    System.out.println("fab_cancel_tracking");
+                    break;
+                case R.id.fab_switch_to_friend:
+                    buttons.toggle(false);
+                    System.out.println("fab_switch_to_friend");
+                    break;
+                case R.id.fab_switch_to_me:
+                    buttons.toggle(false);
+                    System.out.println("fab_switch_to_me");
+                    break;
+                case R.id.fab_navigate:
+                    buttons.toggle(false);
+                    System.out.println("fab_navigate");
+                    break;
+                case R.id.fab_show_us:
+                    buttons.toggle(false);
+                    System.out.println("fab_show_us");
+                    break;
+                case R.id.fab_messages:
+                    buttons.toggle(false);
+                    System.out.println("fab_messages");
+                    break;
+                case R.id.fab_split_screen:
+                    buttons.toggle(false);
+                    System.out.println("fab_split_screen");
+                    break;
+            }
+        }
+    };
+
+    private void adjustButtonsPositions() {
+        ViewGroup v1 = (ViewGroup) mapFragment.getView();
+        ViewGroup v2 = (ViewGroup) v1.getChildAt(0);
+        ViewGroup v3 = (ViewGroup) v2.getChildAt(2);
+        View myLocationButton = v3.getChildAt(0);
+
+        myLocationButton.setVisibility(View.VISIBLE);
+        myLocationButton.setEnabled(true);
+        myLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(cameras.size()>0){
+                    cameras.get(0).setOrientation(cameras.get(0).getPreviousOrientation()).update();
+
+                    System.out.println("onMyLocationButtonClick:"+cameras.get(0).getPreviousOrientation());
+                }
+            }
+        });
+
+        View zoomButtons =  v3.getChildAt(2);
+        int positionWidth = zoomButtons.getLayoutParams().width;
+        int positionHeight = zoomButtons.getLayoutParams().height;
+
+        RelativeLayout.LayoutParams zoomParams = new RelativeLayout.LayoutParams(positionWidth,positionHeight);
+        int margin = positionWidth/5;
+        zoomParams.setMargins(margin, 0, 0, margin);
+        zoomParams.addRule(RelativeLayout.BELOW, myLocationButton.getId());
+        zoomParams.addRule(RelativeLayout.ALIGN_LEFT, myLocationButton.getId());
+        zoomButtons.setLayoutParams(zoomParams);
+    }
+
+    public GoogleMap.OnMarkerClickListener onMarkerClickListener = new GoogleMap.OnMarkerClickListener() {
+        @Override
+        public boolean onMarkerClick (Marker marker) {
+            MyMarker m = null;
+            for(MyMarker x:markers){
+                if(marker.getId().equals(x.getMarker().getId())){
+                    m = x;
+                    break;
+                }
+            }
+            if(m != null){
+                m.getMyCamera().nextOrientation();
+            }
+            return true;
+        }
+    };
+
+
+    GoogleMap.OnMapClickListener onMapClickListener = new GoogleMap.OnMapClickListener() {
+        @Override
+        public void onMapClick(LatLng latLng) {
+            System.out.println("ONMAPCLICK");
         }
     };
 
---
+++ app/src/main/java/ru/wtg/whereaminow/helpers/MyMarker.java
@@ -0,0 +1,245 @@
+package ru.wtg.whereaminow.helpers;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Interpolator;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+
+import ru.wtg.whereaminow.R;
+
+/**
+ * Created by tujger on 9/18/16.
+ */
+public class MyMarker {
+
+
+    private static GoogleMap map;
+    private Context context;
+    private MyCamera myCamera;
+
+    private Marker marker;
+    private int color;
+    private ArrayList<Location> locations;
+    private Location location;
+
+    private boolean draft;
+
+    public MyMarker(){
+        locations = new ArrayList<>();
+        color = Color.BLUE;
+    }
+
+    public MyMarker(Context context){
+        this();
+        setContext(context);
+    }
+
+    public void showDraft(Location location){
+        System.out.println("showDraft:"+location);
+        if(location == null) return;
+
+        setLocation(location);
+        setDraft(true);
+        createMarker();
+        /*CircleOptions circleOptions = new CircleOptions()
+                .center(new LatLng(location.getLatitude(), location.getLongitude())).radius(location.getAccuracy())
+                .fillColor(Color.CYAN).strokeColor(Color.BLUE).strokeWidth(2f);
+        circle = map.addCircle(circleOptions);*/
+
+        update();
+    }
+
+    public void addLocation(Location location) {
+        locations.add(location);
+        setLocation(location);
+        update();
+    }
+
+    private void createMarker(){
+        Drawable drawable;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            drawable = context.getResources().getDrawable(R.drawable.navigation_marker,context.getTheme());
+        } else {
+            drawable = context.getResources().getDrawable(R.drawable.navigation_marker);
+        }
+        if(isDraft()) {
+            drawable.setColorFilter(new ColorMatrixColorFilter(getColorMatrix(Color.GRAY)));
+        } else {
+            drawable.setColorFilter(new ColorMatrixColorFilter(getColorMatrix(color)));
+        }
+
+        Canvas canvas = new Canvas();
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+
+        marker = map.addMarker(new MarkerOptions()
+                .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                .rotation(location.getBearing())
+                .anchor(0.5f, 0.5f)
+                .flat(true)
+//                .title("Melbourne")
+//                .snippet("Population: 4,137,400")
+                .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
+//        marker.showInfoWindow();
+
+    }
+
+    public void update(){
+        if(locations.size()==0) return;
+
+        if(marker != null && isDraft()){
+            marker.remove();
+            setDraft(false);
+            createMarker();
+        } else if(marker == null){
+            if(myCamera != null){
+                myCamera.setZoom(MyCamera.DEFAULT_ZOOM);
+            }
+            createMarker();
+        }
+
+        final LatLng startPosition = marker.getPosition();
+        final LatLng finalPosition = new LatLng(location.getLatitude(), location.getLongitude());
+
+        final float startRotation = marker.getRotation();
+        final float finalRotation = location.getBearing();
+
+        final Handler handler = new Handler();
+        final long start = SystemClock.uptimeMillis();
+        final Interpolator interpolator = new AccelerateDecelerateInterpolator();
+        final float durationInMs = 500;
+
+        handler.post(new Runnable() {
+            long elapsed;
+            float t, v;
+
+            @Override
+            public void run() {
+                elapsed = SystemClock.uptimeMillis() - start;
+                t = elapsed / durationInMs;
+                v = interpolator.getInterpolation(t);
+
+                LatLng currentPosition = new LatLng(
+                        startPosition.latitude*(1-t)+finalPosition.latitude*t,
+                        startPosition.longitude*(1-t)+finalPosition.longitude*t);
+
+                float rot = v * finalRotation + (1 - v) * startRotation;
+
+                marker.setRotation(-rot > 180 ? rot/2 : rot);
+                marker.setPosition(currentPosition);
+
+                if (t < 1) {
+                    handler.postDelayed(this, 16);
+                }
+            }
+        });
+    }
+
+    private float[] getColorMatrix(int color) {
+        switch (color) {
+            case Color.RED:
+                return new float[]{
+                        1, 0, 0, 0, 255, //red
+                        0, 1, 0, 0, 0, //green
+                        0, 0, 1, 0, 0, //blue
+                        0, 0, 0, 1, 0 //alpha
+                };
+            case Color.GREEN:
+                return new float[]{
+                        1, 0, 0, 0, 0, //red
+                        0, 1, 0, 0, 255, //green
+                        0, 0, 1, 0, 0, //blue
+                        0, 0, 0, 1, 0 //alpha
+                };
+            case Color.BLUE:
+                return new float[]{
+                        1, 0, 0, 0, 0, //red
+                        0, 1, 0, 0, 0, //green
+                        0, 0, 1, 0, 255, //blue
+                        0, 0, 0, 1, 0 //alpha
+                };
+            case Color.GRAY:
+                return new float[]{
+                        1, 0, 0, 0, 0, //red
+                        0, 1, 0, 0, 0, //green
+                        0, 0, 1, 0, 0, //blue
+                        0, 0, 0, .5f, 0 //alpha
+                };
+            default:
+                return new float[]{
+                        1, 0, 0, 0, 0, //red
+                        0, 1, 0, 0, 0, //green
+                        0, 0, 1, 0, 0, //blue
+                        0, 0, 0, 1, 0 //alpha
+                };
+        }
+    }
+
+    private void setContext(Context context) {
+        this.context = context;
+    }
+
+    public static GoogleMap getMap() {
+        return map;
+    }
+
+    public static void setMap(GoogleMap map) {
+        MyMarker.map = map;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
+    private boolean isDraft() {
+        return draft;
+    }
+
+    private void setDraft(boolean draft) {
+        this.draft = draft;
+    }
+
+    private void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation(){
+        return location;
+    }
+
+
+    public MyCamera getMyCamera() {
+        return myCamera;
+    }
+
+    public MyMarker setMyCamera(MyCamera myCamera) {
+        this.myCamera = myCamera;
+        return this;
+    }
+
+    public Marker getMarker(){
+        return marker;
+    }
+
+}
