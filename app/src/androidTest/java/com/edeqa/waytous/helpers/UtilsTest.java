3bc927d01a8818ba6dcd281c61d49795f8adc816
eduardm <eduardm@pantheon-inc.com>
2017-09-08 18:00:12 -0400

    WTU-273 Test case for Utils

---
+++ app/src/androidTest/java/com/edeqa/waytous/helpers/UtilsTest.java
@@ -0,0 +1,115 @@
+package com.edeqa.waytous.helpers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by eduardm on 008, 9/8/2017.
+ */
+public class UtilsTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getEncryptedHash() throws Exception {
+
+    }
+
+    @Test
+    public void getEncryptedHash1() throws Exception {
+
+    }
+
+    @Test
+    public void getColorMatrix() throws Exception {
+
+    }
+
+    @Test
+    public void jsonToLocation() throws Exception {
+
+    }
+
+    @Test
+    public void locationToJson() throws Exception {
+
+    }
+
+    @Test
+    public void renderDrawable() throws Exception {
+
+    }
+
+    @Test
+    public void renderDrawable1() throws Exception {
+
+    }
+
+    @Test
+    public void renderBitmap() throws Exception {
+
+    }
+
+    @Test
+    public void renderBitmap1() throws Exception {
+
+    }
+
+    @Test
+    public void getUrl() throws Exception {
+
+    }
+
+    @Test
+    public void getUrl1() throws Exception {
+
+    }
+
+    @Test
+    public void getUnique() throws Exception {
+
+    }
+
+    @Test
+    public void normalizeLocation() throws Exception {
+
+    }
+
+    @Test
+    public void deserializeFromString() throws Exception {
+
+    }
+
+    @Test
+    public void serializeToString() throws Exception {
+
+    }
+
+    @Test
+    public void findPoint() throws Exception {
+
+    }
+
+    @Test
+    public void latLng() throws Exception {
+
+    }
+
+    @Test
+    public void formatLengthToLocale() throws Exception {
+
+    }
+
+    @Test
+    public void reduce() throws Exception {
+
+    }
+
+    @Test
+    public void adaptedSize() throws Exception {
+
+    }
+
+}
